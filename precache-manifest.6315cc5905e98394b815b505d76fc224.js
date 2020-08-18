5f68de95fb889dda21b6e318c9cfd1a95d314770
ventoji <vento.ji@gmail.com>
2020-08-18 12:25:39 +0200

    Updates

---
+++ precache-manifest.6315cc5905e98394b815b505d76fc224.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd77ea44f6add448605a3b9fc934595b",
+    "url": "/react-mosnters/index.html"
+  },
+  {
+    "revision": "6ffdf0847ebeafb0130c",
+    "url": "/react-mosnters/static/css/main.4e398d8d.chunk.css"
+  },
+  {
+    "revision": "79ad37e630052ae7a401",
+    "url": "/react-mosnters/static/js/2.2c32a551.chunk.js"
+  },
+  {
+    "revision": "6ffdf0847ebeafb0130c",
+    "url": "/react-mosnters/static/js/main.1c4b93fc.chunk.js"
+  },
+  {
+    "revision": "01fde5aea3bfdfd53b9c",
+    "url": "/react-mosnters/static/js/runtime~main.030779e9.js"
+  }
+]);
